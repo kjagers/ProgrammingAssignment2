c05cc2d09bc8729b0944cce67a299feae0a84fb3
kjagers <kjagers01@gmail.com>
2015-11-22 08:29:02 -0500

    Update

--- cachematrix.R
+++ cachematrix.R
@@ -2,33 +2,36 @@
 ##get the value of the matrix
 ##set the value of the inverse
 ##get the value of the inverse
+## Example Data! X = matrix(c(1, 1, 1, 3, 4, 3, 3, 3, 4), nrow = 3, ncol = 3)
+## test <- makeCacheMatrix(X)
+## cacheSolve(test)
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
+  i <- NULL
   set <- function(y) {
     x <<- y
-    m <<- NULL
+    i <<- NULL
   }
   get <- function() x
-  setsolve <- function(solve) m <<- solve
-  getsolve <- function() m
+  setsolution <- function(solve) i <<- solve
+  getsolution <- function() i
   list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
+       setsolution = setsolution,
+       getsolution = getsolution)
 }
 
 ## looks for existing solution in cache and prints
 ## if no solution exists, solves, and caches
 
 cacheSolve <- function(x, ...) {
-  m <- x$getsolve()
-  if(!is.null(m)) {
+  i <- x$getsolution()
+  if(!is.null(i)) {
     message("getting cached data")
-    return(m)
+    return(i)
   }
   data <- x$get()
-  m <- solve(data, ...)
-  x$setsolve(m)
-  m
+  i <- solve(data)
+  x$setsolution(i)
+  i
 }
 
